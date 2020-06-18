f187d81f223835310f66dc24ce4ad9e76b810e5b
harry524483 <harinder.singh@sainsburys.co.uk>
2020-06-18 23:12:41 +0100

    Setup monorepo with typescript and serverless

---
+++ packages/server/src/handler.ts
@@ -0,0 +1,11 @@
+const handler = async (event: Event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: event,
+    }),
+  };
+};
+
+export default handler;
